960bcecd292146479d5d722692da673cb79973b6
andrew <andrew_mokryj@ukr.net>
2023-02-01 11:16:11 +0200

    draft: use rxdart merged stream

--- lib/providers/amplify_api_provider.dart
+++ lib/providers/amplify_api_provider.dart
@@ -1,6 +1,7 @@
 import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:radency_internship_project_2/models/AppTransaction.dart';
+import 'package:rxdart/rxdart.dart';
 
 class AmplifyApiProvider{
   AmplifyStreamGroup getTransactionsStreams() {
@@ -19,6 +20,29 @@ class AmplifyApiProvider{
 
     return AmplifyStreamGroup(onTransactionAdded: addOperation, onTransactionChanged: changeOperation, onTransactionDeleted: deleteOperation);
   }
+
+  ///
+  Stream<AmplifyApiEvent> getTransactionsStream() {
+    final Stream<AmplifyApiEvent> addOperation = Amplify.API.subscribe(
+      ModelSubscriptions.onCreate(AppTransaction.classType),
+      onEstablished: () => print('Subscription on added established'),
+    ).map((event) => AmplifyApiEvent(event, AmplifyResponseType.add));
+    final Stream<AmplifyApiEvent> changeOperation = Amplify.API.subscribe(
+      ModelSubscriptions.onUpdate(AppTransaction.classType),
+      onEstablished: () => print('Subscription on updated established'),
+    ).map((event) => AmplifyApiEvent(event, AmplifyResponseType.change));
+    final Stream<AmplifyApiEvent> deleteOperation = Amplify.API.subscribe(
+      ModelSubscriptions.onDelete(AppTransaction.classType),
+      onEstablished: () => print('Subscription on deleted established'),
+    ).map((event) => AmplifyApiEvent(event, AmplifyResponseType.delete));
+
+    final mergedStream = MergeStream([
+      addOperation,
+      changeOperation,
+      deleteOperation,
+    ]);
+    return mergedStream;
+  }
 }
 
 class AmplifyStreamGroup {
@@ -28,3 +52,15 @@ class AmplifyStreamGroup {
 
   AmplifyStreamGroup({required this.onTransactionAdded, required this.onTransactionChanged, required this.onTransactionDeleted});
 }
+
+
+///
+class AmplifyApiEvent {
+  final GraphQLResponse<AppTransaction> response;
+  final AmplifyResponseType type;
+
+  AmplifyApiEvent(this.response, this.type);
+}
+
+enum AmplifyResponseType{add, change, delete}
+
